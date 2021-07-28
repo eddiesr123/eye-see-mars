49ea7fc075f83742610f70869148256d9a534c97
eddiesr123 <eddiesr1234@gmail.com>
2021-07-28 16:18:50 -0400

    added constants and added a custom axios instance

---
+++ src/constants.js
@@ -0,0 +1,38 @@
+// API Key
+export const API_KEY = "api_key=SJtLy0wNppl58fwcHcMcFKo9i4tkASgJlgLSmXkJ";
+
+// API Path for photos taken by Curiosity
+export const CURIOSITY_PHOTOS = "/rovers/curiosity/photos?";
+
+// API Path for photos taken by Opportunity
+export const OPPORTUNITY_PHOTOS = "/rovers/opportunity/photos?";
+
+// API Path for photos taken by Spirit
+export const SPIRIT_PHOTOS = "/rovers/spirit/photos?";
+
+// Filter photos by Camera
+export const CAMERA_KEY = "camera=";
+
+// Front Hazard Avoidance
+export const FHAZ = "FHAZ";
+
+// Rear Hazard Avoidance
+export const RHAZ = "RHAZ";
+
+// Mast
+export const MAST = "MAST";
+
+// Navigation Cameras
+export const NAVCAM = "NAVCAM";
+
+// Query by Earth Date YYYY-MM-DD
+export const EARTH_DATE_KEY = "earth_date=";
+
+// Limit query to 25 photos per call with page -- int
+export const PAGE_KEY = "page=";
+
+
+
+
+
+
